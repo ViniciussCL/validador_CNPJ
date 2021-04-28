94364b36faf655eda98aa66bf33c1509fa31ab26
Marcus Vinicius Cristino <82963330+ViniciussCL@users.noreply.github.com>
2021-04-28 17:00:12 -0300

    Update cnpj_main.py

--- cnpj_main.py
+++ cnpj_main.py
@@ -4,6 +4,6 @@ cnpj_original = input('Insira seu CPF: ')
 
 
 if funcao_cnpj.validador(cnpj_original):
-    print(f'{cnpj_original} é válido')
+    print(f'O CNPJ:{cnpj_original} inserido é válido')
 else:
-    print(f'{cnpj_original} é inválido')
+    print(f'O CNPJ:{cnpj_original} inserido é inválido')
